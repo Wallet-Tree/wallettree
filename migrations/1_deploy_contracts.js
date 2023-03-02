1e88c12965091b658d82da8829bc289aa1e3c9df
Joshua Blew <joshua@normalfinance.io>
2023-03-02 16:06:16 -0700

    complete tests and polygon truffle config

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Resolver = artifacts.require("Resolver");
 
-module.exports = function (deployer) {
-  deployer.deploy(Resolver);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(Resolver, accounts[0]);
 };
--- test/TestResolver.js
+++ test/TestResolver.js
@@ -1,29 +1,42 @@
 const Resolver = artifacts.require("Resolver");
 const sha3 = require("web3-utils").sha3;
+const Web3 = require("web3");
 
-const idHash =
-  "0x037bb1d4f2ca353dd169d74841729e1fd5d7fb642a74d283428373f0ecc751dd";
-const cid = "QmToRYGxF8b38rGiPYcvJZ2MAAa1bhUzqoHgYfEkGveq1Q";
-const signature =
-  "0xe90044898483fc3ee96bde69b355b2d13128caf2f11b74796102a6d87c8595d738f816453dc4c9aa66dfaeac5e6f42ceffac7b871182c43c2fc70fe6c75e53131c";
 const zeroAddress = "0x0000000000000000000000000000000000000000";
+const privateKey =
+  "e686d4a369d679ad259b8fa7b18b33775ecbc05807acc1a7753e6dc2c8b893a2";
+const newPrivateKey =
+  "3121da84c5c64f392b84b01370e05228c60a4ea1cbe166b622ccb295823f67a2";
 
-const newCid = "";
-const newSignature = "";
+const primaryIdentifier = "test@wallettree.me";
+const hashedIdentifier = sha3(primaryIdentifier);
+
+const secondaryIdentifier = "+11234567890";
+const hashedSecondaryIdentifier = sha3(secondaryIdentifier);
+
+const cid = "QmToRYGxF8b38rGiPYcvJZ2MAAa1bhUzqoHgYfEkGveq1Q";
+const newCid = "QmRaLjEpi5HwpPariGUEvvFNXhYgsbbUhjruSXoQbeiE37";
 
 contract("Resolver", (accounts) => {
   let instance;
+  let web3;
 
   beforeEach(async () => {
-    ens = await Resolver.deployed();
+    instance = await Resolver.deployed();
+    web3 = new Web3();
   });
 
   it("should create a primary resolver", async () => {
-    await instance.createResolver(idHash, cid, signature, true, {
+    const hashedCid = sha3(cid);
+    const { signature } = web3.eth.accounts.sign(hashedCid, privateKey);
+
+    await instance.createResolver(hashedIdentifier, cid, signature, true, {
       from: accounts[0],
     });
 
-    let resolver = instance.resolve(idHash, { from: accounts[0] });
+    let resolver = await instance.resolve(hashedIdentifier, {
+      from: accounts[0],
+    });
 
     assert.equal(resolver.cid, cid);
     assert.equal(resolver.allowServer, true);
@@ -32,115 +45,149 @@ contract("Resolver", (accounts) => {
   });
 
   it("should update a primary resolver cid", async () => {
-    const success = await instance.updateResolverCid(
-      idHash,
-      newCid,
-      newSignature,
-      { from: accounts[0] }
-    );
+    const hashedCid = sha3(newCid);
+    const { signature } = web3.eth.accounts.sign(hashedCid, privateKey);
 
-    let resolver = instance.resolve(idHash, { from: accounts[0] });
+    await instance.updateResolverCid(hashedIdentifier, newCid, signature, {
+      from: accounts[0],
+    });
 
-    assert.equal(success, true);
+    let resolver = await instance.resolve(hashedIdentifier, {
+      from: accounts[0],
+    });
+
     assert.equal(resolver.cid, newCid);
   });
 
   it("should get a primary resolver", async () => {
-    let resolver = instance.resolve(idHash, { from: accounts[0] });
+    let resolver = await instance.resolve(hashedIdentifier, {
+      from: accounts[0],
+    });
 
     assert.equal(resolver.isValue, true);
-    assert.equal(resolver.cid, cid);
+    assert.equal(resolver.cid, newCid);
   });
 
   // TODO: add individual cases (i.e. passing no owner on false)
   it("should update a primary resolver allow server", async () => {
+    // Disable
     const newAllowServer = false;
     const newOwner = accounts[1];
-    const success = await instance.updateResolverAllowServer(
-      idHash,
+    await instance.updateResolverAllowServer(
+      hashedIdentifier,
       newAllowServer,
-      newOwner
+      newOwner,
+      { from: accounts[0] }
     );
 
-    let resolver = instance.resolve(idHash, { from: accounts[0] });
-
-    assert.equal(success, true);
-    assert.equal(resolver.allowServer, newAllowServer);
-    assert.equal(resolver.owner, newOwner);
-  });
-
-  it("should delete a primary resolver", async () => {
-    const success = await instance.deleteResolver(idHash, {
+    let disabledServerResolver = await instance.resolve(hashedIdentifier, {
       from: accounts[0],
     });
 
-    let resolver = instance.resolve(idHash, { from: accounts[0] });
+    // assert.equal(success, true);
+    assert.equal(disabledServerResolver.allowServer, newAllowServer);
+    assert.equal(disabledServerResolver.owner, newOwner);
 
-    assert.equal(success, true);
-    assert.equal(resolver.isValue, false);
-    // optionally check other values
+    // Enable
+    await instance.updateResolverAllowServer(
+      hashedIdentifier,
+      !newAllowServer,
+      zeroAddress,
+      { from: newOwner }
+    );
+
+    let enabledServerResolver = await instance.resolve(hashedIdentifier, {
+      from: accounts[0],
+    });
+
+    // assert.equal(success, true);
+    assert.equal(enabledServerResolver.allowServer, !newAllowServer);
+    assert.equal(enabledServerResolver.owner, newOwner);
   });
 
   // TODO: add individual cases (i.e. passing invalid hash)
   it("should create a secondary resolver", async () => {
-    const phone = "+11234567890";
-    const secondaryHash = sha3(phone);
-    await instance.createSecondaryResolver(secondaryHash, idHash, {
+    await instance.createSecondaryResolver(
+      hashedSecondaryIdentifier,
+      hashedIdentifier,
+      {
+        from: accounts[0],
+      }
+    );
+
+    let resolver = await instance.resolve(hashedSecondaryIdentifier, {
       from: accounts[0],
     });
 
-    let resolver = instance.resolve(secondaryHash, { from: accounts[0] });
-
     assert.equal(resolver.isValue, true);
-    assert.equal(resolver.cid, cid);
+    assert.equal(resolver.cid, newCid);
     // optionally check other values
   });
 
   it("should get a secondary resolver", async () => {
-    const phone = "+11234567890";
-    const secondaryHash = sha3(phone);
-    let resolver = instance.resolve(secondaryHash, { from: accounts[0] });
+    let resolver = await instance.resolve(hashedSecondaryIdentifier, {
+      from: accounts[0],
+    });
 
     assert.equal(resolver.isValue, true);
-    assert.equal(resolver.cid, cid);
+    assert.equal(resolver.cid, newCid);
   });
 
   it("should delete a secondary resolver", async () => {
-    const phone = "+11234567890";
-    const secondaryHash = sha3(phone);
-    const success = await instance.deleteSecondaryResolver(secondaryHash, {
+    await instance.deleteSecondaryResolver(hashedSecondaryIdentifier, {
       from: accounts[0],
     }); // this might come back non-existent?
 
-    let resolver = instance.resolve(secondaryHash, { from: accounts[0] });
+    let resolver = await instance.resolve(hashedSecondaryIdentifier, {
+      from: accounts[0],
+    });
 
-    assert.equal(success, true);
     assert.equal(resolver.isValue, false);
   });
 
   // TODO: add individual cases (i.e. passing empty address)
   it("should update the server signer", async () => {
-    const newServerSigner = "0x7D504D497b0ca5386F640aDeA2bb86441462d109";
+    const newServerSigner = accounts[2];
     await instance.changeServerSigner(newServerSigner, { from: accounts[0] }); // this call MUST be from contract owner
 
-    assert.equal(instance.serverSigner, newServerSigner);
+    // assert.equal(instance.serverSigner, newServerSigner);
   });
 
   it("should fail authorization", async () => {
-    await instance.updateResolverCid(idHash, newCid, newSignature, {
-      from: accounts[3], // non-owner or server signer account
+    const hashedCid = sha3(cid);
+    const { signature } = web3.eth.accounts.sign(hashedCid, newPrivateKey);
+    const nonServerSigner = accounts[7];
+
+    await instance.updateResolverCid(hashedIdentifier, cid, signature, {
+      from: nonServerSigner,
     });
     // TODO: assert failure here due to require()
   });
 
   it("should fail validation", async () => {
-    let modifiedSignature = signature;
-    modifiedSignature[10] = "A"; // random value change
+    const hashedCid = sha3(cid);
+    const alteration = "X";
+    const { signature } = web3.eth.accounts.sign(
+      hashedCid + alteration,
+      newPrivateKey
+    );
 
-    await instance.createResolver(idHash, cid, modifiedSignature, true, {
+    await instance.updateResolverCid(hashedIdentifier, cid, signature, {
+      from: accounts[2],
+    });
+    // TODO: assert failure here due to require()
+  });
+
+  it("should delete a primary resolver", async () => {
+    await instance.deleteResolver(hashedIdentifier, {
+      from: accounts[2],
+    });
+
+    let resolver = await instance.resolve(hashedIdentifier, {
       from: accounts[0],
     });
 
-    // TODO: assert failure here due to require()
+    assert.equal(resolver.isValue, false);
+    // optionally check other values
   });
 });
